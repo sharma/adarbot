185e42ed61b8b26bdcebc80339485bb1abacd860
Ankit Sharma <me@ankit.party>
2019-05-13 16:51:05 -0700

    Even more formatting changes
    Market cap cleaned up

--- bot.ts
+++ bot.ts
@@ -23,27 +23,40 @@ bot.on('message', function(event) {
             }
         })
         .then(function (response) {
-            var mcap = response.data.marketCap.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-            var change;
-            var changePercent;
-            if (response.data.change < 0) {
-              change = c.red(response.data.change.toFixed(2));
-              changePercent = c.red(100 * response.data.changePercent.toFixed(3))
-            }
-            else if (response.data.change > 0) {
-              change = c.green(response.data.change.toFixed(2));
-              changePercent = c.green(100 * response.data.changePercent.toFixed(4))
-            }
-            else {
-              change = response.data.change.toFixed(2);
-              changePercent = 100 * response.data.changePercent.toFixed(4);
-            }
-            event.reply(response.data.symbol + 
-            " | " + c.bold(response.data.companyName) + 
-            " | $" + response.data.latestPrice.toFixed(2) + 
-            " | Change: $" + change + 
-            " (" + changePercent +  
-            " %) | MCAP: $" + mcap);
+
+          var change;
+          var changePercent;
+          if (response.data.change < 0) {
+            change = c.red(response.data.change.toFixed(2));
+            changePercent = c.red("(" + (100 * response.data.changePercent.toFixed(4)) + "%)");
+          }
+          else if (response.data.change > 0) {
+            change = c.green("+" + response.data.change.toFixed(2));
+            changePercent = c.green("(+" + (100 * response.data.changePercent.toFixed(4)) + "%)");
+          }
+          else {
+            change = response.data.change.toFixed(2);
+            changePercent = "(" + (100 * response.data.changePercent.toFixed(4)) + "%)";
+          }
+          event.reply(
+            response.data.symbol + 
+          " | " + c.bold(response.data.companyName) + 
+          " | $" + response.data.latestPrice.toFixed(2) + 
+          " " + change + 
+          " " + changePercent +  
+          " | MCAP: $" + formattedMCAP(response.data.marketCap));
         });
     }
 });
+
+function formattedMCAP(num) {
+  if (num === null) { return null; } // terminate early
+  if (num === 0) { return '0'; } // terminate early
+  var fixed = (!fixed || fixed < 0) ? 1 : fixed; // number of decimal places to show
+  var b = (num).toPrecision(2).split("e"), // get power
+      k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+      c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed), // divide by power
+      d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
+      e = d + ['', 'K', 'M', 'B', 'T'][k]; // append power
+  return e;
+}
