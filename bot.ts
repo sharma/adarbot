edee609d5ae8bc8f654731cf51266af580a0368a
Ankit Sharma <me@ankit.party>
2019-05-13 17:22:58 -0700

    Cleaned up if/elses

--- bot.ts
+++ bot.ts
@@ -1,7 +1,8 @@
 const IRC = require('irc-framework');
 const axios = require('axios');
-const bot = new IRC.Client();
 const c = require('irc-colors');
+
+const bot = new IRC.Client();
 
 bot.connect({
 	host: 'irc.synirc.net',
@@ -15,8 +16,8 @@ bot.on('registered', function(event) {
 
 bot.on('message', function(event) {
     if (event.message.match(/^\,st(ock)?/)) {
-        var to_join = event.message.split(' ');
-        var query = 'https://cloud.iexapis.com/stable/stock/' + to_join[1] + '/quote';
+        const to_join = event.message.split(' ');
+        const query = 'https://cloud.iexapis.com/stable/stock/' + to_join[1] + '/quote';
         axios.get(query, {
           params: {
             token: process.env.API_KEY
@@ -24,20 +25,21 @@ bot.on('message', function(event) {
         })
         .then(function (response) {
 
-          var change;
-          var changePercent;
+          var change = response.data.change.toFixed(2);
+          var changePercent = "(" + (100 * response.data.changePercent.toFixed(4)) + "%)";
+
           if (response.data.change < 0) {
-            change = c.red(response.data.change.toFixed(2));
-            changePercent = c.red("(" + (100 * response.data.changePercent.toFixed(4)) + "%)");
+            change = c.red(change);
+            changePercent = c.red(changePercent);
           }
           else if (response.data.change > 0) {
-            change = c.green("+" + response.data.change.toFixed(2));
-            changePercent = c.green("(+" + (100 * response.data.changePercent.toFixed(4)) + "%)");
+            change = c.green(change);
+            changePercent = c.green(changePercent);
           }
           else {
-            change = response.data.change.toFixed(2);
-            changePercent = "(" + (100 * response.data.changePercent.toFixed(4)) + "%)";
+            changePercent = c.stripColors(changePercent);
           }
+
           event.reply(
             response.data.symbol + 
           " | " + c.bold(response.data.companyName) + 
@@ -53,7 +55,7 @@ function formattedMCAP(num) {
   if (num === null) { return null; } // terminate early
   if (num === 0) { return '0'; } // terminate early
   var fixed = (!fixed || fixed < 0) ? 1 : fixed; // number of decimal places to show
-  var b = (num).toPrecision(2).split("e"), // get power
+  const b = (num).toPrecision(2).split("e"), // get power
       k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
       c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed), // divide by power
       d = c < 0 ? c : Math.abs(c), // enforce -0 is 0
