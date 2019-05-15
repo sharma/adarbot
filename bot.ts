e4faca25e16841cc97a8c6ee33c357df3305d9f9
Ankit Sharma <me@ankit.party>
2019-05-15 15:44:03 -0700

    Vars to lets

--- bot.ts
+++ bot.ts
@@ -24,8 +24,8 @@ bot.on('message', function(event) {
       }
     })
     .then(function (response) {
-      var change = response.data.change.toFixed(2);
-      var changePercent = "(" + (100 * response.data.changePercent.toFixed(4)) + "%)";
+      let change = response.data.change.toFixed(2);
+      let changePercent = "(" + (100 * response.data.changePercent.toFixed(4)) + "%)";
 
       if (response.data.change < 0) {
         change = c.red(change);
@@ -51,7 +51,7 @@ bot.on('message', function(event) {
 function formattedMCAP(num) {
   if (num === null) { return null; } // terminate early
   if (num === 0) { return '0'; } // terminate early
-  var fixed = (!fixed || fixed < 0) ? 1 : fixed; // number of decimal places to show
+  let fixed = 1; // number of decimal places to show
   const b = (num).toPrecision(2).split("e"), // get power
       k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
       c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3) ).toFixed(1 + fixed), // divide by power
