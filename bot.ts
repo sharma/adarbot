8fe0cf4a5379a277f5ff7cde8ae2998c15c7cbc8
Ankit Sharma <me@ankit.party>
2019-05-15 15:42:27 -0700

    De-uglifying tabs

--- bot.ts
+++ bot.ts
@@ -15,41 +15,38 @@ bot.on('registered', function(event) {
 });
 
 bot.on('message', function(event) {
-    if (event.message.match(/^\,st(ock)?/)) {
-        const to_join = event.message.split(' ');
-        const query = 'https://cloud.iexapis.com/stable/stock/' + to_join[1] + '/quote';
-        axios.get(query, {
-          params: {
-            token: process.env.API_KEY
-            }
-        })
-        .then(function (response) {
+  if (event.message.match(/^\,st(ock)?/)) {
+    const to_join = event.message.split(' ');
+    const query = 'https://cloud.iexapis.com/stable/stock/' + to_join[1] + '/quote';
+    axios.get(query, {
+      params: {
+        token: process.env.API_KEY
+      }
+    })
+    .then(function (response) {
+      var change = response.data.change.toFixed(2);
+      var changePercent = "(" + (100 * response.data.changePercent.toFixed(4)) + "%)";
 
-          var change = response.data.change.toFixed(2);
-          var changePercent = "(" + (100 * response.data.changePercent.toFixed(4)) + "%)";
+      if (response.data.change < 0) {
+        change = c.red(change);
+        changePercent = c.red(changePercent);
+      }
+      else if (response.data.change > 0) {
+        change = c.green(change);
+        changePercent = c.green(changePercent);
+      }
 
-          if (response.data.change < 0) {
-            change = c.red(change);
-            changePercent = c.red(changePercent);
-          }
-          else if (response.data.change > 0) {
-            change = c.green(change);
-            changePercent = c.green(changePercent);
-          }
-
-          event.reply(
-            response.data.symbol + 
-            " | " + c.bold(response.data.companyName) + 
-            " | $" + response.data.latestPrice.toFixed(2) + 
-            " " + change + 
-            " " + changePercent +  
-            " | MCAP: $" + formattedMCAP(response.data.marketCap)
-          );
-        }
+      event.reply(
+        response.data.symbol + 
+        " | " + c.bold(response.data.companyName) + 
+        " | $" + response.data.latestPrice.toFixed(2) + 
+        " " + change + 
+        " " + changePercent +  
+        " | MCAP: $" + formattedMCAP(response.data.marketCap)
       );
-    }
+    });
   }
-);
+});
 
 function formattedMCAP(num) {
   if (num === null) { return null; } // terminate early
