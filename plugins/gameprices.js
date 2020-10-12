d2d7a97ecf9baa740e69677bd600b8931679126f
Ankit Sharma <me@ankit.party>
2020-10-12 03:20:32 -0700

    Fixed bug in game prices plugin

--- plugins/gameprices.js
+++ plugins/gameprices.js
@@ -6,6 +6,7 @@ module.exports.search = async function gameprices(event) {
   let game = event.message.replace(",gp ", "");
   game = encodeURIComponent(game);
   let { price, gameName, shopName } = "";
+  let continueSearch = 0;
   const ITAD_API_KEY = process.env.ITAD_API_KEY;
   const query = `https://api.isthereanydeal.com/v02/search/search/?key=${ITAD_API_KEY}&q=${game}&country=US`;
   await axios
@@ -14,12 +15,16 @@ module.exports.search = async function gameprices(event) {
       plain = response.data.data.results[0].plain;
       console.log(plain);
       gameName = response.data.data.results[0].title;
+      continueSearch = 1;
     })
     .catch(error => {
       console.log(error);
       event.reply(event.nick + ": Error finding game.");
-      return;
     });
+
+  if (continueSearch === 0) {
+    return;
+  }
 
 const query2 = `https://api.isthereanydeal.com/v01/game/prices/?key=${ITAD_API_KEY}&plains=${plain}&country=US`;
 
