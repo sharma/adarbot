f4577e99692d9f100fdba74b8a0dbf75e83180c0
Benjamin Herr <git@1d6.org>
2021-09-05 02:21:07 -0700

    reddit: use await instead of then/catch

--- plugins/reddit.js
+++ plugins/reddit.js
@@ -12,57 +12,58 @@ function hasLink(message) {
 }
 
 async function summarizeLinks(event, censoredStrings) {
+    try {
+        return await summarizeLinksInner(event, censoredStrings);
+    } catch (err) {
+        console.log("reddit.summarizeLinks() failed: ", err);
+    }
+}
+
+async function summarizeLinksInner(event, censoredStrings) {
     const matches = event.message.match(PATTERN);
     if (!matches.length) {
         return;
     }
 
     const url = matches[0].split('?')[0] + ".json";
-    await axios
-        .get(url)
-        .then(response => {
-            const {
-                subreddit_name_prefixed,
-                title
-            } = response.data[0].data.children[0].data;
+    const response = await axios.get(url);
+    const {
+        subreddit_name_prefixed,
+        title
+    } = response.data[0].data.children[0].data;
 
-            const { id, body, author } = response.data[1].data.children[0].data;
-            const commentIDSize = 7;
-            const commentIDFromURL = url
-                .replace(/\/$/, "")
-                .slice(-commentIDSize);
+    const { id, body, author } = response.data[1].data.children[0].data;
+    const commentIDSize = 7;
+    const commentIDFromURL = url
+        .replace(/\/$/, "")
+        .slice(-commentIDSize);
 
-            let parsedTitle = he.decode(title.replace(/\r?\n|\r/g, " "));
-            let subreddit = c.bold(subreddit_name_prefixed.replace(/\r?\n|\r/g, " "));
+    let parsedTitle = he.decode(title.replace(/\r?\n|\r/g, " "));
+    let subreddit = c.bold(subreddit_name_prefixed.replace(/\r?\n|\r/g, " "));
 
-            if (commentIDFromURL === id) {
-                const commentLength = 330 - (author.length + subreddit_name_prefixed.length + 1);
-                const commentBody = he.decode(body.replace(/\r?\n|\r/g, " ").substring(0, commentLength));
+    if (commentIDFromURL === id) {
+        const commentLength = 330 - (author.length + subreddit_name_prefixed.length + 1);
+        const commentBody = he.decode(body.replace(/\r?\n|\r/g, " ").substring(0, commentLength));
 
-                let comment = '"' + commentBody;
+        let comment = '"' + commentBody;
 
-                if (body.length > commentLength) {
-                    comment = comment + "...";
-                }
+        if (body.length > commentLength) {
+            comment = comment + "...";
+        }
 
-                comment = comment + '"';
-                for (let i = 0; i < (censoredStrings.length + 3); i++) {
-                    if (comment.includes(censoredStrings[i])) {
-                        return;
-                    }
-                }
-                event.reply(subreddit + " | " + comment + ` — /u/${author}`);
-            } else {
-                for (let i = 0; i < censoredStrings.length; i++) {
-                    if (parsedTitle.includes(censoredStrings[i])) {
-                        return;
-                    }
-                }
-                event.reply(subreddit + " | " + parsedTitle);
+        comment = comment + '"';
+        for (let i = 0; i < (censoredStrings.length + 3); i++) {
+            if (comment.includes(censoredStrings[i])) {
+                return;
             }
-        })
-
-        .catch(error => {
-            console.log(error);
-        });
+        }
+        event.reply(subreddit + " | " + comment + ` — /u/${author}`);
+    } else {
+        for (let i = 0; i < censoredStrings.length; i++) {
+            if (parsedTitle.includes(censoredStrings[i])) {
+                return;
+            }
+        }
+        event.reply(subreddit + " | " + parsedTitle);
+    }
 }
